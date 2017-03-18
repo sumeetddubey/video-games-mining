547061cae6144d226d3014a6d3b77ab9f458676b
Sumeet Dubey <sumeetddubey@gmail.com>
2017-03-17 23:24:47 -0400

    conducted some exploratory analysis over top publishers and their sales

--- explore1.py
+++ explore1.py
@@ -1,19 +1,24 @@
+import operator
+import matplotlib.pyplot as plt
+
 games={}
 genres=set()
 publishers=set()
 
 with open('dataset/games.csv', 'r', encoding = "ISO-8859-1") as ip:
-	for line in ip:
-		line=line.strip()
-		line=line.split(',')
-		publishers.add(line[4])
-		genres.add(line[3])
-		if line[0] in games:
-			# print(line[0])
-			games[line[0]].extend(line)
-		else:
-			games[line[0]] = line
+    next(ip)
+    for line in ip:
+        line=line.strip()
+        line=line.split(',')
+        publishers.add(line[4])
+        genres.add(line[3])
+        if line[0] in games:
+            # print(line[0])
+            games[line[0]].extend(line)
+        else:
+            games[line[0]] = line
 
 print('Number of unique games', len(games.keys()))
 print('Number of genres', len(genres))
 print('Number of publishers', len(publishers))
+
